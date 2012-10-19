7531541fa85adec61f2b57ebc38dc56f72b2d245
unknown <cjramey@wit-xdvm-074.MILLARDPS.NET>
2012-10-19 14:11:48 -0600

    CSS & Javascript files

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+		<script type="text/javascript" src="techtools.js"></script>
+	
 		<title>TechTools &raquo; Millard West High School</title>
 	</head>
 	<body>
+		<div id="wrapper">
+			<header>
+			</header>
+			<nav>
+			</nav>
+			<div id="main">
+			</div>
+			<footer>
+				<p></p>
+			</footer>
+		</div>
 	</body>
 </html>
