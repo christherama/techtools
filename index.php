e08f8c530d3b69aaa05e3373a9285173e01b4caf
chrisramey <cjramey@gmail.com>
2012-10-19 18:26:04 -0500

    Added jQuery & Bootstrap libraries

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="styles.css" />
-		<script type="text/javascript" src="techtools.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/styles.css" />
+		
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/techtools.js"></script>
 	
 		<title>TechTools &raquo; Millard West High School</title>
 	</head>
